44109d15c320acc4726d4436e1e2db181722d78f
Thomas Bousquet <thomas.bousquet@gmail.com>
2017-01-12 17:02:32 -0500

    adding account security check

--- routes/candidate.js
+++ routes/candidate.js
@@ -6,7 +6,7 @@ let _                       = require('lodash');
 
 // Get all candidates
 router.get('/', (req, res, next)    => {
-    Candidate.find().then((candidates) => res.send(candidates)).catch((e) => res.status(400).send(e))
+    Candidate.find({ _account : req.user._account }).then(candidates => res.send(candidates)).catch(e => res.status(400).send(e));
 });
 
 // Get candidate
@@ -14,7 +14,7 @@ router.get('/:id', (req, res, next) => {
     let id = req.params.id;
     if(!ObjectID.isValid(id)) res.status(400).send();
 
-    Candidate.findById(id).then((candidate) => res.send(candidate)).catch((e) => res.status(400).send(e));
+    Candidate.findById(id).then(candidate => res.send(candidate.toJSON())).catch(e => res.status(400).send(e));
 });
 
 // Create candidate
@@ -23,19 +23,29 @@ router.post('/', (req, res, next) => {
     let candidate = new Candidate(req.body);
     candidate._account = req.user._account;
 
-    candidate.save().then((doc) => res.send(doc)).catch((e) => res.status(400).send(e));
+    candidate.save().then(candidate => res.send(candidate.toJSON())).catch(e => res.status(400).send(e));
 });
 
 // Update candidate
 router.patch('/:id', (req, res, next) => {
 
-    // @todo, find document, access control
     let id      = req.params.id;
     let body    = _.pick(req.body, ['firstname', 'lastname', 'email', 'birthdate', 'phone', 'availableAt', 'availableUntil']);
 
     if(!ObjectID.isValid(id)) res.status(400).send();
 
-    Candidate.findByIdAndUpdate(id, { $set : body }, { new : true }).then((candidate) => res.send(candidate)).catch((e) => res.status(400).send(e));
+    Candidate.findById(id)
+        .then((candidate) => {
+
+            if(!candidate) res.status(400).send();
+            if(candidate._account.toString() != req.user._account._id.toString()) {
+                res.status(403).send();
+            }
+
+            return Candidate.findByIdAndUpdate(id, { $set : body }, { new : true });
+        })
+        .then(candidate => res.send(candidate.toJSON()))
+        .catch(e => res.status(400).send(e));
 });
 
 // Delete candidate
@@ -44,7 +54,7 @@ router.delete('/:id', (req, res, next) => {
     let id = req.params.id;
     if(!ObjectID.isValid(id)) res.status(400).send();
 
-    Candidate.findByIdAndRemove(id).then((candidate) => res.send(candidate)).catch((e) => res.status(400).send());
+    Candidate.findByIdAndRemove(id).then(candidate => res.send(candidate.toJSON())).catch(e => res.status(400).send());
 });
 
 module.exports = router;
