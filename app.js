9b1f10a9283e4df6106e4bd585366dcba249baf9
Thomas Bousquet <thomas.bousquet@gmail.com>
2017-01-25 15:02:01 -0500

    test

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.set('view options', { layout : 'email/layout.hbs' });
 
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
